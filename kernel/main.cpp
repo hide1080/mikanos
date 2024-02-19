1670fce77e572ef055594155ef75c3d5e8c2ea2e
hide1080 <hide1080@gmail.com>
2024-02-20 07:50:35 +0900

    17章b （ルートディレクトリのみ）lsコマンド

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -16,6 +16,7 @@
 #include "acpi.hpp"
 #include "asmfunc.h"
 #include "console.hpp"
+#include "fat.hpp"
 #include "font.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
@@ -189,6 +190,7 @@ extern "C" void KernelMainNewStack(
   InitializeMemoryManager(memory_map);
   InitializeInterrupt();
 
+  fat::Initialize(volume_image);
   InitializePCI();
 
   InitializeLayer();
@@ -214,23 +216,6 @@ extern "C" void KernelMainNewStack(
   usb::xhci::Initialize();
   InitializeKeyboard();
   InitializeMouse();
-
-  uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
-  printk("Volume Image:\n");
-
-  for (int i = 0; i < 16; i++) {
-    printk("%04x:", i * 16);
-    for (int j = 0; j < 8; j++) {
-      printk(" %02x", *p);
-      p++;
-    }
-    printk(" ");
-    for (int j = 0; j < 8; j++) {
-      printk(" %02x", *p);
-      p++;
-    }
-    printk("\n");
-  }
 
   char str[128];
 

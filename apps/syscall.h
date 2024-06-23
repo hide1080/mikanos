a7cd6d7d3ad049e5b4dc586b9686cd8ba73fbaa3
hide1080 <hide1080@gmail.com>
2024-06-23 12:31:15 +0900

    21章d コード整理

---
+++ apps/syscall.h
@@ -0,0 +1,17 @@
+#include <cstdint>
+#include "../kernel/logger.hpp"
+
+extern "C" {
+
+  struct SyscallResult {
+    uint64_t value;
+    int error;
+  };
+
+  SyscallResult SyscallLogString(LogLevel level, const char* message);
+
+  SyscallResult SyscallPutString(int fd, char* s, size_t len);
+
+  void SyscallExit(int exit_code);
+
+} // extern "C"

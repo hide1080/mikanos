7f35e16d482e38c155056f244f1f8c0fa6cd4708
hide1080 <hide1080@gmail.com>
2024-07-23 07:07:37 +0900

    23章e キー押下イベントとブロック崩しゲーム

--- apps/newlib_support.c
+++ apps/newlib_support.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <signal.h>
 #include <stdint.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -14,8 +15,17 @@ int fstat(int fd, struct stat* buf) {
   return -1;
 }
 
+pid_t getpid(void) {
+  return 0;
+}
+
 int isatty(int fd) {
   errno = EBADF;
+  return -1;
+}
+
+int kill(pid_t pid, int sig) {
+  errno = EPERM;
   return -1;
 }
 

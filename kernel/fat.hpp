2603a350cf5a7462e0f38240353d7d5a531e4d69
hide1080 <hide1080@gmail.com>
2024-11-25 18:24:39 +0900

    26章a 標準入力

--- kernel/fat.hpp
+++ kernel/fat.hpp
@@ -8,6 +8,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include "file.hpp"
 
 namespace fat {
   struct BPB {
@@ -148,10 +149,10 @@ namespace fat {
    */
   size_t LoadFile(void* buf, size_t len, const DirectoryEntry& entry);
 
-  class FileDescriptor {
+  class FileDescriptor : public ::FileDescriptor {
     public:
       explicit FileDescriptor(DirectoryEntry& fat_entry);
-      size_t Read(void* buf, size_t len);
+      size_t Read(void* buf, size_t len) override;
 
     private:
       DirectoryEntry& fat_entry_;

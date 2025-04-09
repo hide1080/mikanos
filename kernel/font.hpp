c17134bb3783473d29032c4657d52b99f4ed16d7
hide1080 <hide1080@gmail.com>
2025-04-09 09:14:26 +0900

    28章a 日本語と文字コード

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <cstdint>
+#include <utility>
 #include"graphics.hpp"
 
 void WriteAscii(PixelWriter& writer,
@@ -18,3 +19,14 @@ void WriteString(PixelWriter& writer,
                  Vector2D<int> pos,
                  const char* s,
                  const PixelColor& color);
+
+int CountUTF8Size(uint8_t c);
+
+std::pair<char32_t, int> ConvertUTF8To32(const char* u8);
+
+bool IsHankaku(char32_t c);
+
+void WriteUnicode(PixelWriter& writer,
+                  Vector2D<int> pos,
+                  char32_t c,
+                  const PixelColor& color);
